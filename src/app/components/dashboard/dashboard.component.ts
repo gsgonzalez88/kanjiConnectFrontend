248970ec7aeb72610e0e37bd3bc21c068107d58d
MalenaLucero <luceromalena@hotmail.com>
2021-10-19 21:30:15 -0300

    dashboard disable

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,13 +2,19 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 
+interface Page {
+  page: string,
+  active: boolean,
+  location: string
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  public pages = [
+  public pages: Page[] = [
+    { page: 'review', active: false, location: '' },
     { page: 'review', active: false, location: '/review' },
     { page: 'upload', active: false, location: '/upload' }
   ];
@@ -17,18 +23,20 @@ export class DashboardComponent implements OnInit {
               private location: Location) { }
 
   ngOnInit(): void {
-    const currentLocation = this.location.path()
-    this.setCurrentPageState(this.pages.find(page => page.location === currentLocation)?.page || 'home')
+    const currentLocation = this.location.path();
+    this.setCurrentPageState(this.pages.find(page => page.location === currentLocation))
   }
 
-  navigate(page: string) {
+  navigate(page: Page) {
     this.setCurrentPageState(page)
-    this.router.navigate([`/${page}`]);
+    this.router.navigate([`/${page.location}`]);
   }
 
-  setCurrentPageState(currentPage: string) {
-    this.pages.forEach(page =>
-      page.active = page.page === currentPage
-    )
+  setCurrentPageState(pageData: Page | undefined ) {
+    if (pageData !== undefined) {
+      this.pages.forEach(page => {
+        page.active = page.page === pageData.page
+      })
+    }
   }
 }
