10b2c588a4c6cd7dafa8a06544c31aee5f1a9c60
MalenaLucero <luceromalena@hotmail.com>
2022-01-21 16:32:42 -0300

    shared components to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,26 +12,23 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimeInterceptor } from './shared/interceptors/time.interceptor';
 import { TokenInterceptor } from './shared/interceptors/token.interceptor';
 import { HomeComponent } from './shared/pages/home/home.component';
-import { LoginComponent } from './shared/components/login/login.component';
 import { ReviewComponent } from './study/pages/review/review.component';
 import { ExpressionCardComponent } from './study/components/review-card/review-card.component';
 import { ReviewCounterComponent } from './study/components/review-counter/review-counter.component';
 import { DifficultyPipe } from './study/pipes/difficulty.pipe';
 import { CardFilterComponent } from './study/components/card-filter/card-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainButtonComponent } from './shared/elements/buttons/main-button/main-button.component';
 import { AccentButtonComponent } from './shared/elements/buttons/accent-button/accent-button.component';
 import { WarnButtonComponent } from './shared/elements/buttons/warn-button/warn-button.component';
-import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 import { UploadComponent } from './study/pages/upload/upload.component';
 import { ExpressionFormComponent } from './study/components/expression-form/expression-form.component';
 import { LessonsSelectComponent } from './shared/elements/selects/lessons-select/lessons-select.component';
@@ -40,16 +37,13 @@ import { TypesSelectComponent } from './shared/elements/selects/types-select/typ
 import { IconButtonComponent } from './shared/elements/buttons/icon-button/icon-button.component';
 import { JlptSelectComponent } from './shared/elements/selects/jlpt-select/jlpt-select.component';
 import { TransitivitySelectComponent } from './shared/elements/selects/transitivity-select/transitivity-select.component';
-import { LoadingComponent } from './shared/components/loading/loading.component';
 import { ExpressionCastPipe } from './study/pipes/expression-cast.pipe';
 import { UserKanjiCastPipe } from './study/pipes/user-kanji-cast.pipe';
 import { DifficultySelectComponent } from './shared/elements/selects/difficulty-select/difficulty-select.component';
 import { SourceSelectComponent } from './shared/elements/selects/source-select/source-select.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { DifficultyButtonsComponent } from './study/components/difficulty-buttons/difficulty-buttons.component';
 import { ReviewCardPopupComponent } from './study/components/review-card-popup/review-card-popup.component';
 import { FirstTitlecasePipe } from './study/pipes/first-titlecase.pipe';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 import { StudyModule } from './study/study.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -57,17 +51,14 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     ReviewComponent,
     ExpressionCardComponent,
     ReviewCounterComponent,
     DifficultyPipe,
     CardFilterComponent,
-    HeaderComponent,
     MainButtonComponent,
     AccentButtonComponent,
     WarnButtonComponent,
-    DashboardComponent,
     UploadComponent,
     ExpressionFormComponent,
     LessonsSelectComponent,
@@ -76,16 +67,13 @@ import { SharedModule } from './shared/shared.module';
     IconButtonComponent,
     JlptSelectComponent,
     TransitivitySelectComponent,
-    LoadingComponent,
     ExpressionCastPipe,
     UserKanjiCastPipe,
     DifficultySelectComponent,
     SourceSelectComponent,
-    FooterComponent,
     DifficultyButtonsComponent,
     ReviewCardPopupComponent,
     FirstTitlecasePipe,
-    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
@@ -105,7 +93,6 @@ import { SharedModule } from './shared/shared.module';
     MatDividerModule,
     MatChipsModule,
     MatDialogModule,
-    MatProgressSpinnerModule,
     StudyModule,
     SharedModule,
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,45 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoginComponent } from './components/login/login.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoadingComponent,
+    LoginComponent,
+    SpinnerComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ], exports: [
+    DashboardComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoadingComponent,
+    LoginComponent,
+    SpinnerComponent,
   ]
 })
 export class SharedModule { }
